af0958830b302a571601858401fc34c099418fa0
Elan Ness-Cohn <ness.cohn.elan@gmail.com>
2020-12-10 11:00:54 -0600

    Add GitHub links to DESCRIPTION

---
+++ R/fasano_franceschini_test.R
@@ -0,0 +1,74 @@
+#' Fasano Franceschini Test
+#'
+#' Computes the 2-D Kolmogorov-Smirnov two-sample test as described by Fasano and Franceschini (1987).
+#' Code adapted from Press, W. H., Teukolsky, S. A., Vetterling, W. T.,, Flannery, B. P. (2007). Numerical Recipes 3rd Edition: The Art of Scientific Computing. Cambridge University Press. ISBN: 0521880688
+#'
+#' @param S1 a [n by 2] `data.frame` of x and y coordinates of sample 1
+#' @param S2 a [n by 2] `data.frame` of x and y coordinates of sample 2
+#' @param nBootstrap `numeric` number of bootstrapped samples to be generated for computing the empirical p-value (note this procedure is slow and computationally expensive on the order of nBootStrap*O(n^2). Default is set to 0.
+#' If nBootstrap is 0, the Fasano Franceschini distributional approximation is used for defining the p-value. See Fasano and Franceschini test (1987) for details.
+#' @param cores a `numeric` defining the number of cores to use of processing
+#'
+#' @return the 2-D ks statistic and p-value
+#' @export
+#'
+#' @references{
+#' \itemize{
+#' \item{Fasano, G., Franceschini, A. (1987)<DOI:10.1093/mnras/225.1.155>. A multidimensional version of the Kolmogorov-Smirnov test. Monthly Notices of the Royal Astronomical Society 225:155-170.}}
+#' \item{Peacock J.A. (1983) <DOI:10.1093/mnras/202.3.615>. Two-dimensional goodness-of-fit testing in astronomy. Monthly Notices of the Royal Astronomical Society 202:615-627.}}
+#' \item{Press, W. H., Teukolsky, S. A., Vetterling, W. T.,, Flannery, B. P. (2007). Numerical Recipes 3rd Edition: The Art of Scientific Computing. Cambridge University Press. ISBN: 0521880688}}
+#' }
+#'
+#' @examples
+fasano.franceschini.test <- function(S1, S2, nBootstrap = 0, cores = 2) {
+  start <- Sys.time()
+  # determine number of samples in each data set
+  n1 <- dim(S1)[1]
+  n2 <- dim(S2)[1]
+
+  # is there a better way to do the S1 from outer to inner function?
+  # determine max difference assuming first sample as origins
+  d1 <- get_d_stat(originSamples = S1, S1 = S1, S2 = S2, cores = cores)
+  # determine max difference assuming second sample as origins
+  d2 <- get_d_stat(originSamples = S2, S1 = S1, S2 = S2, cores = cores)
+
+  # average KS stat
+  D <- (d1 + d2) / 2
+
+  # if bootstramp is enabled, compute the bootstrapped null
+  if (nBootstrap > 0) {
+    x_marg <- c(S1[, 1], S2[, 1])
+    y_marg <- c(S1[, 2], S2[, 2])
+
+    d <- mclapply(X = 1:nBootstrap, mc.cores = cores, FUN = function(i) {
+      S1_resample <- data.frame(
+        x = sample(x = x_marg, size = n1, replace = T),
+        y = sample(x = y_marg, size = n1, replace = T)
+      )
+      S2_resample <- data.frame(
+        x = sample(x = x_marg, size = n2, replace = T),
+        y = sample(x = y_marg, size = n2, replace = T)
+      )
+      get_d_stat(originSamples = S1_resample, S1 = S1_resample, S2 = S2_resample, cores = 1)
+    })
+
+    # count the number of bootstrapped d stats that are larger than the observed
+    pval <- sum(unlist(d) > D) / nBootstrap
+    ksStat <- D
+    print(Sys.time() - start)
+    return(list(ksStat = ksStat, epval = pval))
+  } else {
+    # Use the fasano.franceschini distributional approximation
+    # average Rsquared
+    r1 <- stats::cor(S1[, 1], S1[, 2])
+    r2 <- stats::cor(S2[, 1], S2[, 2])
+    rr <- (r1^2 + r2^2) / 2
+
+    # compute KS stat
+    n <- n1 * n2 / (n1 + n2)
+    ksStat <- sqrt(n) * D / (1 + sqrt(1 - rr) * (0.25 - 0.75 / sqrt(n)))
+    pval <- ksCDF(ksStat)
+    print(Sys.time() - start)
+    return(list(ksStat = ksStat, pval = pval))
+  }
+}
---
+++ R/interal_functions_FasFran.R
@@ -0,0 +1,115 @@
+#' Quad Count
+#'
+#' Counts the frequency of points in the four quadrants - starting from the upper right going counter clockwise. Quadrants defined by the origin points x and y.
+#'
+#' Code adapted from Press, W. H., Teukolsky, S. A., Vetterling, W. T.,, Flannery, B. P. (2007). Numerical Recipes 3rd Edition: The Art of Scientific Computing. Cambridge University Press. ISBN: 0521880688
+#'
+#'
+#' @param x_origin a `numeric` defining the x coordinate of the origin defining the 4 quadrants
+#' @param y_origin a `numeric` defining the y coordinate of the origin defining the 4 quadrants
+#' @param x a `vector` of `numeric` x coordinates
+#' @param y a `vector` of `numeric` y coordinates
+#'
+#' @return a `vector` of frequencies of the number of points in each of the four quadrants defined by the origin point
+#' @export
+#'
+#' @references{
+#' \itemize{
+#'    \item{Press, W. H., Teukolsky, S. A., Vetterling, W. T.,, Flannery, B. P. (2007). Numerical Recipes 3rd Edition: The Art of Scientific Computing. Cambridge University Press. ISBN: 0521880688}}
+#' }
+#'
+#'
+#'
+#' @examples
+quadCount <- function(x_origin, y_origin, x, y) {
+  n <- length(x)
+  nq1 <- 0
+  nq2 <- 0
+  nq3 <- 0
+  nq4 <- 0
+
+  # think about less than or equal to?
+  # i.e. ties
+  for (k in 1:n) {
+    if (y[k] >= y_origin) {
+      if (x[k] >= x_origin) {
+        nq1 <- nq1 + 1
+      } else {
+        nq2 <- nq2 + 1
+      }
+    } else {
+      if (x[k] >= x_origin) {
+        nq4 <- nq4 + 1
+      } else {
+        nq3 <- nq3 + 1
+      }
+    }
+  }
+
+  freqQuad1 <- nq1 / n
+  freqQuad2 <- nq2 / n
+  freqQuad3 <- nq3 / n
+  freqQuad4 <- nq4 / n
+
+  return(c(freqQuad1, freqQuad2, freqQuad3, freqQuad4))
+}
+
+#' Get KS Stat
+#'
+#' Loop through each row as(i.e. data point)
+#' and defines D stat as the largest difference between the quadfrequencies, looping though each point in the sample as the origin
+#'
+#' Code adapted from Press, W. H., Teukolsky, S. A., Vetterling, W. T.,, Flannery, B. P. (2007). Numerical Recipes 3rd Edition: The Art of Scientific Computing. Cambridge University Press. ISBN: 0521880688
+#'
+#' @param S1 a [n by 2] `data.frame` of x and y coordinates of sample 1
+#' @param S2 a [n by 2] `data.frame` of x and y coordinates of sample 2
+#' @param cores a `numeric` defining the number of cores to use of processing
+#' @param originSamples a [n by 2] `data.frame` of x and y coordinates that defines the origins data points.
+#'
+#' @return a `numeric` defining the D stat with the largest difference between the quad frequencies, after checking each point as the origin
+#' @export
+#'
+#' @examples
+get_d_stat <- function(originSamples, S1, S2, cores = 1) {
+
+  d <- 0
+  n <- dim(originSamples)[1]
+  dList <- mclapply(X = 1:n, mc.cores = cores, FUN = function(j){
+    quadct_S1 <- quadCount(originSamples[j, 1], originSamples[j, 2], S1[, 1], S1[, 2])
+    quadct_S2 <- quadCount(originSamples[j, 1], originSamples[j, 2], S2[, 1], S2[, 2])
+    d <- max(
+      abs(quadct_S1[1] - quadct_S2[1]),
+      abs(quadct_S1[2] - quadct_S2[2]),
+      abs(quadct_S1[3] - quadct_S2[3]),
+      abs(quadct_S1[4] - quadct_S2[4])
+    )
+  })
+  return(max(unlist(dList)))
+}
+
+
+#' KS probability
+#'
+#' p-value of getting the specified 2-D KS stat
+#' Code adapted from Press, W. H., Teukolsky, S. A., Vetterling, W. T.,, Flannery, B. P. (2007). Numerical Recipes 3rd Edition: The Art of Scientific Computing. Cambridge University Press. ISBN: 0521880688
+#'
+#'
+#' @param lambda
+#'
+#' @return a `numeric` defining the p-value of observing the given 2-D KS stat
+#' @export
+#'
+#' @examples
+ksCDF <- function(lambda) {
+  eps1 <- 0.001
+  eps2 <- 1E-8
+
+  range <- seq(1, 100)
+  terms <- (-1)^(range - 1) * exp(-2 * range^2 %*% t(lambda^2))
+  sums <- 2 * colSums(terms)
+  pterms <- abs(terms)
+  prev_pterms <- rbind(0, pterms[-nrow(pterms), , drop = FALSE])
+  converged <- apply(pterms <= eps1 * prev_pterms | pterms <= eps2 * sums, 2L, any)
+  sums[!converged] <- 1
+  sums
+}
